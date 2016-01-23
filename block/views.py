ac5ea0307ddb25f08a1c08cc46c7bf4e7f9c67ac
wangxuan <wangxuan8901@126.com>
2016-01-23 21:03:03 +0800

    change

--- block/views.py
+++ block/views.py
@@ -6,4 +6,4 @@ from models import Block
 # Create your views here.
 def block_list(request):
     blocks = Block.objects.all().order_by("-id")
-    return render_to_response("block_list.html", {"block": blocks})
+    return render_to_response("block_list.html", {"blocks": blocks})
