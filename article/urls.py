340ee0928716f3fdae167dd2cd617fc7caa8c29c
wangxuan <wangxuan8901@126.com>
2016-01-28 11:53:29 +0800

    change

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
         url(r'^lists/(?P<block_id>\d+)', "article.views.article_list", name="article_list"),
+        url(r'^$', "article.views.article_list", name="article_list"),
 ]
