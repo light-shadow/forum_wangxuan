042426977994de8bcd09aaff9e6dffb62b66dd6d
wangxuan <wangxuan8901@126.com>
2016-02-27 21:40:32 +0800

    change

--- comment/views.py
+++ comment/views.py
@@ -1,9 +1,11 @@
 # coding: utf-8
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from article.models import Article
-from models import Comment
 from utils.response import json_response
+from models import Comment
+from utils.paginator import paginate_queryset
 
 
 @login_required
@@ -19,6 +21,13 @@ def create_comment(request):
     return json_response({})
 
 
-@login_required
-def comment_list(request):
-    pass
+def comment_list(request, article_id):
+    article_id = int(article_id)
+    comment_page_no = int(request.GET.get("comment_page_no", 1))
+    article = Article.objects.get(id=article_id)
+    comments = Comment.objects.filter(article=article).order_by("-last_update_timestamp")
+
+    object_list, pagination_data = paginate_queryset(comments, comment_page_no)
+    return render_to_response("article_detail.html", {"comments": object_list, "article": article,
+                              "pagination": pagination_data},
+                              context_instanc=RequestContext(request))
