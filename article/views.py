6969a540a02a82472c2b2b331a4ee1a07d99f85d
wangxuan <wangxuan8901@126.com>
2016-02-29 20:49:11 +0800

    change

--- article/views.py
+++ article/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from block.models import Block
 from models import Article
+from comment.models import Comment
 from utils.paginator import paginate_queryset
 
 
@@ -45,6 +46,11 @@ def create_article(request, block_id):
 
 def article_detail(request, article_id):
     article_id = int(article_id)
+    comment_page_no = int(request.GET.get("comment_page_no", 1))
     article = Article.objects.get(id=article_id)
-    return render_to_response("article_detail.html", {"article": article},
+    comments = Comment.objects.filter(article=article).order_by("-last_update_timestamp")
+
+    object_list, pagination_data = paginate_queryset(comments, comment_page_no)
+    return render_to_response("article_detail.html", {"comments": object_list, "article": article,
+                              "pagination": pagination_data},
                               context_instance=RequestContext(request))
--- comment/views.py
+++ comment/views.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 from article.models import Article
 from utils.response import json_response
 from models import Comment
-from utils.paginator import paginate_queryset
 
 
 @login_required
@@ -21,13 +18,6 @@ def create_comment(request):
     return json_response({})
 
 
+@login_required
 def comment_list(request, article_id):
-    article_id = int(article_id)
-    comment_page_no = int(request.GET.get("comment_page_no", 1))
-    article = Article.objects.get(id=article_id)
-    comments = Comment.objects.filter(article=article).order_by("-last_update_timestamp")
-
-    object_list, pagination_data = paginate_queryset(comments, comment_page_no)
-    return render_to_response("article_detail.html", {"comments": object_list, "article": article,
-                              "pagination": pagination_data},
-                              context_instanc=RequestContext(request))
+    pass
