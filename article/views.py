c9e4ce80a2479fc1c1f7db0ba917c7c474e30353
wangxuan <wangxuan8901@126.com>
2016-03-02 19:36:04 +0800

    change

--- article/views.py
+++ article/views.py
@@ -46,11 +46,10 @@ def create_article(request, block_id):
 
 def article_detail(request, article_id):
     article_id = int(article_id)
-    comment_page_no = int(request.GET.get("comment_page_no", 1))
+    page_no = int(request.GET.get("comment_page_no", 1))
     article = Article.objects.get(id=article_id)
     comments = Comment.objects.filter(article=article).order_by("-last_update_timestamp")
-
-    object_list, pagination_data = paginate_queryset(comments, comment_page_no)
-    return render_to_response("article_detail.html", {"comments": object_list, "article": article,
-                              "pagination": pagination_data},
+    comments, pagination_data = paginate_queryset(comments, page_no, cnt_per_page=3)
+    return render_to_response("article_detail.html", {"comments": comments,
+                              "article": article, "pagination": pagination_data},
                               context_instance=RequestContext(request))
